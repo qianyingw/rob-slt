254d420111adfd9f603fa2a5d87fad70c36b7e69
Qianying Wang <w.qianying@outlook.com>
2020-10-11 21:15:42 +0100

    reduce size

--- setup.sh
+++ setup.sh
@@ -8,7 +8,6 @@ headless = true\n\
 \n\
 " > ~/.streamlit/config.toml
 
-curl -c /tmp/cookies "https://drive.google.com/uc?export=download&id=1vztBL9WwUh1vjXOjSfPVgnUTpqdFEcKY" > /tmp/intermezzo.html
-curl -L -b /tmp/cookies "https://drive.google.com$(cat /tmp/intermezzo.html | grep -Po 'uc-download-link" [^>]* href="\K[^"]*' | sed 's/\&amp;/\&/g')" > pth/bert_w0.pth.tar
-
+# curl -c /tmp/cookies "https://drive.google.com/uc?export=download&id=1vztBL9WwUh1vjXOjSfPVgnUTpqdFEcKY" > /tmp/intermezzo.html
+# curl -L -b /tmp/cookies "https://drive.google.com$(cat /tmp/intermezzo.html | grep -Po 'uc-download-link" [^>]* href="\K[^"]*' | sed 's/\&amp;/\&/g')" > pth/bert_w0.pth.tar
 #python -m spacy download en_core_web_sm
