54d3d5adf788792db2a5e9166fe08058550ca1f9
Qianying Wang <w.qianying@outlook.com>
2020-10-11 22:22:50 +0100

    fld

--- setup.sh
+++ setup.sh
@@ -9,5 +9,9 @@ headless = true\n\
 " > ~/.streamlit/config.toml
 
 # curl -c /tmp/cookies "https://drive.google.com/uc?export=download&id=1vztBL9WwUh1vjXOjSfPVgnUTpqdFEcKY" > /tmp/intermezzo.html
-# curl -L -b /tmp/cookies "https://drive.google.com$(cat /tmp/intermezzo.html | grep -Po 'uc-download-link" [^>]* href="\K[^"]*' | sed 's/\&amp;/\&/g')" > pth/bert_w0.pth.tar
-#python -m spacy download en_core_web_sm
+# curl -L -b /tmp/cookies "https://drive.google.com$(cat /tmp/intermezzo.html | grep -Po 'uc-download-link" [^>]* href="\K[^"]*' | sed 's/\&amp;/\&/g')" > pth.zip
+# unzip pth.zip
+
+curl -c /tmp/cookies "https://drive.google.com/uc?export=download&id=1d78CyW664EE6iS438ylSMiJgIUwIiagG" > /tmp/intermezzo.html
+curl -L -b /tmp/cookies "https://drive.google.com$(cat /tmp/intermezzo.html | grep -Po 'uc-download-link" [^>]* href="\K[^"]*' | sed 's/\&amp;/\&/g')" > fld.zip
+unzip fld.zip
