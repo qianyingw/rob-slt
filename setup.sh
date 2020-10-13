1fbb68afacf027ad459e2bf6878221958cd41d96
Qianying Wang <w.qianying@outlook.com>
2020-10-13 11:24:41 +0100

    mute

--- setup.sh
+++ setup.sh
@@ -1,17 +1,10 @@
-mkdir -p ~/.streamlit/
+#mkdir -p ~/.streamlit/
 
-echo "\
-[server]\n\
-port = $PORT\n\
-enableCORS = false\n\
-headless = true\n\
-\n\
-" > ~/.streamlit/config.toml
+#echo "\
+#[server]\n\
+#port = $PORT\n\
+#enableCORS = false\n\
+#headless = true\n\
+#\n\
+#" > ~/.streamlit/config.toml
 
-# curl -c /tmp/cookies "https://drive.google.com/uc?export=download&id=1vztBL9WwUh1vjXOjSfPVgnUTpqdFEcKY" > /tmp/intermezzo.html
-# curl -L -b /tmp/cookies "https://drive.google.com$(cat /tmp/intermezzo.html | grep -Po 'uc-download-link" [^>]* href="\K[^"]*' | sed 's/\&amp;/\&/g')" > pth.zip
-# unzip pth.zip
-
-curl -c /tmp/cookies "https://drive.google.com/uc?export=download&id=1d78CyW664EE6iS438ylSMiJgIUwIiagG" > /tmp/intermezzo.html
-curl -L -b /tmp/cookies "https://drive.google.com$(cat /tmp/intermezzo.html | grep -Po 'uc-download-link" [^>]* href="\K[^"]*' | sed 's/\&amp;/\&/g')" > fld.zip
-unzip fld.zip
